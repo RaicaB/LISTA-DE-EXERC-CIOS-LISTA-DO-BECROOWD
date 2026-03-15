5d593539439100e9b8755266c11e6c9bffab7db1
RaicaB <raicamartins97@gmail.com>
2026-03-15 17:47:38 -0300

    Add files via upload

---
+++ ex1072.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int N = int.Parse(Console.ReadLine());
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int X = int.Parse(Console.ReadLine());
+
+                if (X >= 10 && X <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+            }
+
+            Console.WriteLine($"{dentro} in");
+            Console.WriteLine($"{fora} out");
+    }
+}
+
