ef73043c657ca6c2e761b041cb2460d1ccaec025
RaicaB <raicamartins97@gmail.com>
2026-03-14 21:16:39 -0300

    Add files via upload

---
+++ ex1067.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro entre 1 e 1000:");
+        int x = int.Parse(Console.ReadLine());
+
+        if (x < 1 || x > 1000)
+        {
+            Console.WriteLine("Valor inválido!Digite um número entre 1 e 1000.");
+        }
+        else
+        {
+
+            Console.Write("Os números ímpares são:");
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
+
+
+
