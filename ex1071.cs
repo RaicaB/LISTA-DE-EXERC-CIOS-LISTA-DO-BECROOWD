066ffc284c58935ba147dde20fe6c0b89ee1f10a
RaicaB <raicamartins97@gmail.com>
2026-03-15 17:22:29 -0300

    Add files via upload

---
+++ ex1071.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Digite 2 valores inteiros:");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int soma = 0;
+
+        if (x > y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        for (int i = x + 1; i < y; i++)
+        {
+            if (i % 2 != 0) 
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine(soma);
+    }
+}
