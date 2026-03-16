cf2b9d69f89242e49509a9c4c22e1c23c91197d7
RaicaB <raicamartins97@gmail.com>
2026-03-15 21:02:22 -0300

    Add files via upload

---
+++ ex1073.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número:");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= N; i += 2)
+        {
+            Console.WriteLine($"{i}^2 = {i * i}");
+
+        }
+
+    }
+}
