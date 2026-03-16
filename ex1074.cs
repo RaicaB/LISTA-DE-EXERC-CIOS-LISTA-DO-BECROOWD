567c296f00dfc14ce2070f9f3a862516b4de82d0
RaicaB <raicamartins97@gmail.com>
2026-03-15 22:07:29 -0300

    Add files via upload

---
+++ ex1074.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x == 0)
+            {
+                Console.WriteLine("NULL");
+            }
+            else
+            {
+                string parity = (x % 2 == 0) ? "EVEN" : "ODD";
+                string sign = (x > 0) ? "POSITIVE" : "NEGATIVE";
+
+                Console.WriteLine($"{parity} {sign}");
+            }
+        }
+    }
+}
