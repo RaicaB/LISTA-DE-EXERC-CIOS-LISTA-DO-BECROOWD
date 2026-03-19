e38dd2c6c39ea602953b8d0ff4f2995cea00de43
RaicaB <raicamartins97@gmail.com>
2026-03-19 20:50:56 -0300

    Add files via upload

---
+++ ex1151.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (i == 1)
+                Console.Write(a);
+            else if (i == 2)
+                Console.Write(" " + b);
+            else
+            {
+                int c = a + b;
+                Console.Write(" " + c);
+                a = b;
+                b = c;
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
