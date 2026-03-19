6cb725b8ef21db9a15c4a66ac4e5d9661940b27b
RaicaB <raicamartins97@gmail.com>
2026-03-19 20:01:55 -0300

    Add files via upload

---
+++ ex1097.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 9; i += 2)
+        {
+            int j = 7;
+            for (int k = 0; k < 3; k++)
+            {
+                Console.WriteLine("I=" + i + " J=" + j);
+                j += 2;
+            }
+        }
+    }
+}
